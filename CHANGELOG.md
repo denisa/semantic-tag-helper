010210f2364947ab5063babc93f062876b278740
Denis N. Antonioli <1205618+denisa@users.noreply.github.com>
2022-05-14 17:06:15 -0700

    1: Initial version (#3)

---
+++ CHANGELOG.md
@@ -0,0 +1,23 @@
+# changelog
+
+[![Keep a Changelog](https://img.shields.io/badge/Keep%20a%20Changelog-1.0.0-informational)](https://keepachangelog.com/en/1.0.0/)
+[![Semantic Versioning](https://img.shields.io/badge/Sematic%20Versioning-2.0.0-informational)](https://semver.org/spec/v2.0.0.html)
+![clq validated](https://img.shields.io/badge/clq-validated-success)
+
+Keep the newest entry at top, format date according to ISO 8601: `YYYY-MM-DD`.
+
+Categories, defined in [changemap.json](.github/clq/changemap.json)):
+- _major_ release trigger:
+   - `Changed` for changes in existing functionality.
+   - `Removed` for now removed features.
+- _minor_ release trigger:
+   - `Added` for new features.
+   - `Deprecated` for soon-to-be removed features.
+- _bug-fix_ release trigger:
+   - `Fixed` for any bug fixes.
+   - `Security` in case of vulnerabilities.
+
+
+## [1.0.0] - 2022-05-14
+### Added
+- test/set tag and derived tags
---
+++ action.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -eu
+
+function test() {
+	echo "Testing tag $tag"
+	if git ls-remote --exit-code --tags origin "refs/tags/$tag" >/dev/null 2>&1; then
+	  echo "::error ::tag $tag exists already"
+	  exit 1
+	fi
+}
+
+function set() {
+	echo "Setting tag $tag"
+
+	git config user.name github-actions
+	git config user.email github-actions@github.com
+
+	version="${tag%%-*}"
+	preversion="${tag##*-}"
+	if [ "$version" != "$preversion" ]; then
+	  prefix="${version}-"
+	  tag="$preversion"
+	fi
+
+	tags=()
+	before=''
+	until [ "$before" = "$tag" ]; do
+	  tags+=("${prefix:-}${tag}")
+	  git tag "${tags[-1]}"
+	  before="$tag"
+	  tag="${tag%.*}"
+	done
+
+	# ensure this tag doesn't exist
+	git push origin "${tags[0]}"
+	# move all other tags
+	git push origin --force "${tags[@]:1}"
+}
+
+case "$mode" in
+  test)
+    test
+    ;;
+  set)
+    set
+    ;;
+   *)
+   echo "::error ::Mode undefined, must be one of (test|set)"
+   exit 1
+   ;;
+esac
